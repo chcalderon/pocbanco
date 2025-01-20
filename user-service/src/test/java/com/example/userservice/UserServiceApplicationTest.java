50bc282fa259a07bbc3b636e96c804841da43b3b
Christian <chcalderon@gmail.com>
2025-01-19 21:02:55 -0300

    se agrega test clase principal

---
+++ user-service/src/test/java/com/example/userservice/UserServiceApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceApplicationTest {
+
+    @Test
+	void contextLoads() {
+        UserServiceApplication.main(new String[] {});
+	}
+
+}
